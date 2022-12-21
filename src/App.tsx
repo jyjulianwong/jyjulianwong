04fd052a307bf0b2bd11df8ce7ea1bdb6d668478
Julian Wong <julianwong2000@gmail.com>
2022-12-21 14:15:45 +0000

    Use hyperlinks as buttons instead of entire cards, bump version number

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,8 @@ function App() {
             location="London, England, UK"
             duration="Mar 2022 - Jun 2022"
             description="Led an engineering team of 3 creating an all-in-one professional social platform for musicians, as part of a project submission for the Venture Catalyst Challenge at the Imperial Enterprise Lab."
-            redirect="https://github.com/jyjulianwong/Amphere-WebApp-React"
+            redirect="https://github.com/Amphere-Ltd/Amphere-WebApp-React"
+            redirectText="See the repository"
           />
         </Col>
         <Col xs={12}>
@@ -44,6 +45,7 @@ function App() {
             duration="Apr 2022 - Sep 2022"
             description="Developed performance analysis software for elite motorsport teams across 10+ series and championships, enabling teams to visualise trends, make in-race strategic decisions and review driver performance."
             redirect="https://sbgsportssoftware.com/product/racewatch-for-teams"
+            redirectText="Learn more"
             darkened
           />
         </Col>
@@ -57,6 +59,7 @@ function App() {
             duration="Jan 2021 - Aug 2021"
             description="Developed the Biiah (formerly Tchzant) iOS app, enabling people of all backgrounds to learn singing in a gamified format."
             redirect="https://biiah.com"
+            redirectText="Learn more"
           />
         </Col>
       </Row>
