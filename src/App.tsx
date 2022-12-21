48ad4d6e3f5880c4200f853adc5bba4b25fdb2d7
Julian Wong <julianwong2000@gmail.com>
2022-12-21 04:52:59 +0000

    Major UI re-design, update site metadata, bump version number

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FixedAnimatedNavbar from "./components/FixedAnimatedNavbar";
 import ProfileCard from "./components/ProfileCard";
-import PlaceholderCard from "./components/PlaceholderCard";
 import PortfolioCard from "./components/PortfolioCard";
-import {Col, Container, Row} from 'react-bootstrap';
+import {Col, Row} from 'react-bootstrap';
 
 /**
  * Entry point of web app.
@@ -14,63 +13,60 @@ function App() {
   return (
     <div className="App">
       <FixedAnimatedNavbar/>
-      <Container className="my-4">
-        <Row className="g-3">
-          <Col xs={12}>
-            <ProfileCard
-              imageSource="https://avatars.githubusercontent.com/u/33598849"
-              title="Julian Wong"
-              description="Final-Year M.Eng. Computing (Artificial Intelligence and Machine Learning) Student @ Imperial College London"
-            />
-          </Col>
-          <Col xs={12} lg={6}>
-            <PortfolioCard
-              imageSource={require("./assets/amphere.png").default}
-              title="Amphere Web App"
-              company="Amphere Ltd"
-              position="Full-Stack Web Developer"
-              location="London, UK"
-              duration="Mar 2022 - Jun 2022"
-              description="Led an engineering team of 3 creating an all-in-one professional social platform for musicians, as part of a project submission for the Venture Catalyst Challenge at the Imperial Enterprise Lab."
-              redirect="https://github.com/jyjulianwong/Amphere-WebApp-React"
-            />
-          </Col>
-          <Col xs={12} lg={6}>
-            <PortfolioCard
-              imageSource={require("./assets/catapultsports.png").default}
-              title="RaceWatch"
-              company="Catapult Sports"
-              position="Software Engineer Intern"
-              location="London, UK"
-              duration="Apr 2022 - Sep 2022"
-              description="Developed performance analysis software for elite motorsport teams across 10+ series and championships, enabling teams to visualise trends, make in-race strategic decisions and review driver performance."
-              redirect="https://sbgsportssoftware.com/product/racewatch-for-teams"
-            />
-          </Col>
-          <Col xs={12} lg={6}>
-            <PortfolioCard
-              imageSource={require("./assets/biiah.png").default}
-              title="Biiah for iOS"
-              company="Biiah"
-              position="iOS Developer"
-              location="London, UK"
-              duration="Jan 2021 - Aug 2021"
-              description="Developed the Biiah (formerly Tchzant) iOS app, enabling people of all backgrounds to learn singing in a gamified format."
-              redirect="https://biiah.com"
-            />
-          </Col>
-          <Col xs={12} lg={6}>
-            <PlaceholderCard/>
-          </Col>
-        </Row>
-        <Row className="mt-5 mb-3">
-          <Col xs={12}>
-            <p className="terminal-ubuntu">
-              jyjulianwong.github.io/jyjulianwong v{process.env.REACT_APP_VERSION}. Authored by Julian Wong in 2022.
-            </p>
-          </Col>
-        </Row>
-      </Container>
+      <Row className="g-1">
+        <Col xs={12}>
+          <ProfileCard
+            imageSource={require("./assets/julian.png").default}
+            title="Hi! I'm Julian."
+            description="Final-Year M.Eng. Computing (Artificial Intelligence and Machine Learning) Student @ Imperial College London"
+            darkened
+          />
+        </Col>
+        <Col xs={12}>
+          <PortfolioCard
+            imageSource={require("./assets/amphere.png").default}
+            title="Amphere Web App"
+            company="Amphere Ltd"
+            position="Full-Stack Web Developer"
+            location="London, England, UK"
+            duration="Mar 2022 - Jun 2022"
+            description="Led an engineering team of 3 creating an all-in-one professional social platform for musicians, as part of a project submission for the Venture Catalyst Challenge at the Imperial Enterprise Lab."
+            redirect="https://github.com/jyjulianwong/Amphere-WebApp-React"
+          />
+        </Col>
+        <Col xs={12}>
+          <PortfolioCard
+            imageSource={require("./assets/catapultsports.png").default}
+            title="RaceWatch"
+            company="Catapult Sports"
+            position="Software Engineer Intern"
+            location="London, England, UK"
+            duration="Apr 2022 - Sep 2022"
+            description="Developed performance analysis software for elite motorsport teams across 10+ series and championships, enabling teams to visualise trends, make in-race strategic decisions and review driver performance."
+            redirect="https://sbgsportssoftware.com/product/racewatch-for-teams"
+            darkened
+          />
+        </Col>
+        <Col xs={12}>
+          <PortfolioCard
+            imageSource={require("./assets/biiah.png").default}
+            title="Biiah for iOS"
+            company="Biiah"
+            position="iOS Developer"
+            location="London, England, UK"
+            duration="Jan 2021 - Aug 2021"
+            description="Developed the Biiah (formerly Tchzant) iOS app, enabling people of all backgrounds to learn singing in a gamified format."
+            redirect="https://biiah.com"
+          />
+        </Col>
+      </Row>
+      <Row className="mt-5 mb-3 px-3">
+        <Col xs={12}>
+          jyjulianwong.github.io/jyjulianwong v{process.env.REACT_APP_VERSION}.
+          <br/>
+          Authored by Julian Wong in 2022.
+        </Col>
+      </Row>
     </div>
   );
 }
--- src/components/FixedAnimatedNavbar.tsx
+++ src/components/FixedAnimatedNavbar.tsx
@@ -4,13 +4,14 @@ import {RepeatingTimer} from "../models/RepeatingTimer";
 
 function FixedAnimatedNavbar(): JSX.Element {
   const text = "jyjulianwong";
+  const tickInterval = 750;
 
   const [tick, setTick] = useState(0);
 
   useEffect(() => {
     const repeatingTimer = new RepeatingTimer(() => {
       setTick(prevTick => (prevTick + 1) % (text.length + 1));
-    }, 500);
+    }, tickInterval);
 
     return () => {
       repeatingTimer.stop();
@@ -18,10 +19,10 @@ function FixedAnimatedNavbar(): JSX.Element {
   }, []);
 
   return (
-    <Navbar className="terminal-homebrew shadow" expand="lg">
+    <Navbar className="bg-white" expand="lg">
       <Container>
-        <Navbar.Brand href="#" style={{color: "var(--cl-terminal-homebrew-green)"}}>
-          {"> " + text.slice(0, tick) + ((tick % 2 == 0) ? "_" : "")}
+        <Navbar.Brand href="#">
+          {"> " + text + ((tick % 2 == 0) ? "_" : "")}
         </Navbar.Brand>
       </Container>
     </Navbar>
