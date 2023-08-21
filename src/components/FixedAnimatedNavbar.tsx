d0a7da0ce196a2f5b4dfd5d20965241ecf722c5c
Julian Wong <julianwong2000@gmail.com>
2023-08-21 02:16:16 +0100

    Temporarily removed Navbar expand button

--- src/components/FixedAnimatedNavbar.tsx
+++ src/components/FixedAnimatedNavbar.tsx
@@ -41,13 +41,13 @@ function FixedAnimatedNavbar(props: FixedAnimatedNavbarProps): JSX.Element {
             visibility: (tick % 2 == 0) ? "hidden" : "inherit"
           }}>_</p>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav">
-          {/*<Nav className="me-auto">*/}
-          {/*  <Nav.Link href="/">Home</Nav.Link>*/}
-          {/*  <Nav.Link href="/#/top100cars">Top 100 Cars</Nav.Link>*/}
-          {/*</Nav>*/}
-        </Navbar.Collapse>
+        {/*<Navbar.Toggle aria-controls="basic-navbar-nav"/>*/}
+        {/*<Navbar.Collapse id="basic-navbar-nav">*/}
+        {/*  <Nav className="me-auto">*/}
+        {/*    <Nav.Link href="/">Home</Nav.Link>*/}
+        {/*    <Nav.Link href="/#/top100cars">Top 100 Cars</Nav.Link>*/}
+        {/*  </Nav>*/}
+        {/*</Navbar.Collapse>*/}
       </Container>
     </Navbar>
   );
